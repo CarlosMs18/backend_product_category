d15dc29712301dab64a18abaa9983af7c4402384
carlos <carlos.melgarejo.solorzano@hotmail.com>
2023-05-16 11:30:24 -0500

    añdiendo creacion de productos

--- src/main/java/com/melgarejo/backend/controllers/ProductRestController.java
+++ src/main/java/com/melgarejo/backend/controllers/ProductRestController.java
@@ -1,13 +1,20 @@
 package com.melgarejo.backend.controllers;
 
 
+import com.melgarejo.backend.models.Product;
 import com.melgarejo.backend.response.ProductResponseRest;
 import com.melgarejo.backend.services.IProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -22,4 +29,30 @@ public class ProductRestController {
             ResponseEntity<ProductResponseRest> response = service.search();
             return response;
     }
+
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<?> searchById(@PathVariable  Long id){
+        ResponseEntity<ProductResponseRest> response = service.searchById(id);
+        return response;
+    }
+
+
+    @PostMapping("/products")
+    public ResponseEntity<?> crearProducto(@Valid @RequestBody Product product, BindingResult result){
+        Map<String, Object> errors = new HashMap<>();
+        if(result.hasErrors()) {
+
+            List<String> errores = result.getFieldErrors()
+                    .stream()
+                    .map(err -> "El campo '" + err.getField() +"' "+ err.getDefaultMessage())
+                    .collect(Collectors.toList());
+
+            errors.put("errors", errores);
+            return new ResponseEntity<Map<String, Object>>(errors, HttpStatus.BAD_REQUEST);
+        }
+        ResponseEntity<ProductResponseRest> response = service.save(product);
+        return response;
+    }
+
 }
--- src/main/java/com/melgarejo/backend/services/IProductServiceImpl.java
+++ src/main/java/com/melgarejo/backend/services/IProductServiceImpl.java
@@ -1,6 +1,7 @@
 package com.melgarejo.backend.services;
 
 
+import com.melgarejo.backend.dao.ICategoryDao;
 import com.melgarejo.backend.dao.IProductDao;
 import com.melgarejo.backend.models.Category;
 import com.melgarejo.backend.models.Product;
@@ -14,13 +15,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class IProductServiceImpl implements  IProductService {
 
     @Autowired
     private IProductDao productDao;
+
+
+    @Autowired
+    private ICategoryDao categoryDao;
+
     @Override
     @Transactional(readOnly = true)
     public ResponseEntity<ProductResponseRest> search() {
@@ -34,18 +42,68 @@ public class IProductServiceImpl implements  IProductService {
             response.setMetadata("Respuesta nok","-1","Error al consultar");
             e.getStackTrace();
             return new ResponseEntity<ProductResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-        } 
+        }
         return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public ResponseEntity<ProductResponseRest> searchById(Long id) {
-        return null;
+        ProductResponseRest response = new ProductResponseRest();
+        List<Product> list = new ArrayList<>();
+        try{
+            Optional<Product> product = productDao.findById(id);
+            if(product.isPresent()){
+                System.out.println("encontrada");
+                list.add(product.get());
+                response.getProductResponse().setProduct(list);
+                response.setMetadata("Respuesta Ok","00","Producto encontrado");
+            }else{
+                System.out.println("no encontrada");
+                response.setMetadata("Respuesta nok","-1","Producto no encontrada");
+                return new ResponseEntity<ProductResponseRest>(response, HttpStatus.NOT_FOUND);
+            }
+        }catch (DataAccessException e) {
+            response.setMetadata("Respuesta nok", "-1", "Error al consultar");
+            e.getStackTrace();
+            return new ResponseEntity<ProductResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);
+
     }
 
     @Override
     public ResponseEntity<ProductResponseRest> save(Product product) {
-        return null;
+        ProductResponseRest response = new ProductResponseRest();
+        List<Product> list = new ArrayList<>();
+
+        try{
+            Optional<Category> categoryDb = categoryDao.findById(product.getCategory().getId());
+            if(categoryDb.isPresent()){
+                product.setCategory(categoryDb.get());
+            }else{
+                response.setMetadata("Respuesta nok","-1","Categoria no encontrada asociada");
+                return new ResponseEntity<ProductResponseRest>(response, HttpStatus.NOT_FOUND);
+            }
+
+            Product productSaved = productDao.save(product);
+
+            if(productSaved != null){
+                list.add(productSaved);
+                response.getProductResponse().setProduct(list);
+                response.setMetadata("Respuesta Ok", "00","Producto guardado con exito!");
+            }else{
+                response.setMetadata("Respuesta nok", "-1","Producto no guardado");
+                return new ResponseEntity<ProductResponseRest>(response, HttpStatus.BAD_REQUEST);
+            }
+
+        }catch (DataAccessException e) {
+            response.setMetadata("Respuesta nok", "-1", "Error al guardar producto");
+            e.getStackTrace();
+            return new ResponseEntity<ProductResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @Override
